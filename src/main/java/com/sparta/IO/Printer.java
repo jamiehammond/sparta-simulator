82eec7ddcba382626a2483cd7c7a25969e85576b
Ben Middlehurst <bmiddlehurst@spartaglobal.com>
2021-01-05 16:51:45 +0000

    First Merging Printer Complete

--- src/main/java/com/sparta/IO/Printer.java
+++ src/main/java/com/sparta/IO/Printer.java
@@ -5,20 +5,70 @@ import com.sparta.utility.TimeTracker;
 
 public class Printer {
 
-//    public static void greeting(){
-//        System.out.println("Welcome to the Sparta simulator");
-//    }
-//
-//    public static void simulationTime(){
-//        System.out.println("Enter simulation time in Months (Press enter to use default value: "+Settings.getSimulationTime);
-//    }
-//
-//    public static void centreOpeningFrequency(){
-//        System.out.println("Enter centre opening frequency (Press enter to use default value: "+ Settings.getCentreOpeningFrequency);
-//    }
-//
-//    public static void currentMonth(){
-//        System.out.println("Current Month: "+TimeTracker.getCurrentMonth());
-//    }
+    public static void greeting(){
+        System.out.println("Welcome to the Sparta simulator");
+    }
 
+    public static void simulationTime(){
+        System.out.println("Enter simulation time in Months (Press enter to use default value: "+Settings.getSimulationTime);
+    }
+
+    public static void centreOpeningFrequency(){
+        System.out.println("Enter centre opening frequency (Press enter to use default value: "+ Settings.getCentreOpeningFrequency);
+    }
+
+    public static void centreCapacity(){
+        System.out.println("Enter centre capacity (Press enter to use default value: "+ Settings.getCentreCapacity);
+    }
+    public static void newTraineeMin(){
+        System.out.println("Enter lower bound for new trainee generation range (Press enter to use default value: "+ Settings.getNewTraineeMin);
+    }
+
+    public static void newTraineeMax(){
+        System.out.println("Enter upper bound for new trainee generation range (Press enter to use default value: "+ Settings.getNewTraineeMax);
+    }
+
+    public static void newTraineeFrequency(){
+        System.out.println("Enter new trainee frequency in months (Press enter to use default value: "+ Settings.getNewTraineeFrequency);
+    }
+
+    public static void centreAdmittanceMin(){
+        System.out.println("Enter lower bound for centre admittance (Press enter to use default value: "+ Settings.getCentreAdmittanceMin);
+    }
+
+    public static void centreAdmittanceMax(){
+        System.out.println("Enter upper bound for centre admittance (Press enter to use default value: "+ Settings.getCentreAdmittanceMax);
+    }
+
+    public static void centreAdmittanceFrequency(){
+        System.out.println("Enter centre admittance frequency in months (Press enter to use default value: "+ Settings.getCentreAdmittanceFrequency);
+    }
+
+    public static void monthDelay(){
+        System.out.println("Enter how many seconds represent one month (Press enter to use default value: "+ Settings.getMonthDelay);
+    }
+
+    public static void areValuesCorrectToRun(){
+        System.out.println("Are these values correct to run the simulation? (Y/N) ");
+    }
+
+    public static void currentMonth(){
+        System.out.println("Current Month: "+TimeTracker.getCurrentMonth());
+    }
+
+    public static void openCentres(){
+        System.out.println("Number of open centres: "+TimeTracker.getOpenCentres());
+    }
+
+    public static void fullCentres(){
+        System.out.println("Number of full centres: "+TimeTracker.getFullCentres());
+    }
+
+    public static void traineesInTraining(){
+        System.out.println("Number of trainees in training: "+TimeTracker.getTraineesInTraining());
+    }
+
+    public static void traineesInWaitingList(){
+        System.out.println("Number of trainees in waiting list: "+TimeTracker.getTraineesInWaitingList());
+    }
 }
--- src/test/java/com/sparta/IO/PrinterTest.java
+++ src/test/java/com/sparta/IO/PrinterTest.java
@@ -14,141 +14,119 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrinterTest {
-//    private final PrintStream standardOut = System.out;
-//    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-//
-//@BeforeEach
-//public void setUp(){
-//    System.setOut(new PrintStream(outputStreamCaptor));
-//}
-//
-//@Test
-//    void doesGreetingStringMatchExpected(){
-//    Printer.greeting();
-//
-//    Assertions.assertEquals("Welcome to the Sparta simulator", outputStreamCaptor.toString().trim());
-//}
-//
-//    @Test
-//    void doesDefaultSettingsMatchExpected(){
-//        Printer.defaultSettings();
-//        Assertions.assertEquals("The default values are: ", outputStreamCaptor.toString().trim());
-//    }
-//
-//    @Test
-//    void doesSimulationTimeMatchExpected(){
-//        Printer.simulationTime();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getSimulationTime, outputStreamCaptor.toString().trim());
-//    }
-//
-//    @Test
-//    void doesCentreOpeningFrequencyMatchExpected(){
-//        Printer.centreOpeningFrequency();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getCentreOpeningFrequency, outputStreamCaptor.toString().trim());
-//    }
-//
-//    @Test
-//    void doesCentreCapacityMatchExpected(){
-//        Printer.centreCapacity();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getCentreCapacity, outputStreamCaptor.toString().trim());
-//    }
-//
-//    @Test
-//    void doesNewTraineeMinMatchExpected(){
-//        Printer.newTraineeMin();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getTraineeMin, outputStreamCaptor.toString().trim());
-//    }
-//    @Test
-//    void doesNewTraineeMaxMatchExpected(){
-//        Printer.newTraineeMax();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getTraineeMax, outputStreamCaptor.toString().trim());
-//    }
-//
-//    @Test
-//    void doesNewTraineeFrequencyMatchExpected(){
-//        Printer.newTraineeFrequency();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getNewTraineeFrequency, outputStreamCaptor.toString().trim());
-//    }
-//    @Test
-//    void doesCentreAdmittanceMinMatchExpected(){
-//        Printer.centreAdmittanceMin();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getCentreAdmittanceMin, outputStreamCaptor.toString().trim());
-//    }
-//    @Test
-//    void doesCentreAdmittanceMaxMatchExpected(){
-//        Printer.centreAdmittanceMax();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getCentreAdmittanceMax, outputStreamCaptor.toString().trim());
-//    }
-//    @Test
-//    void doesCentreAdmittanceFrequencyMatchExpected(){
-//        Printer.centreAdmittanceFrequency();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getCentreAdmittanceFrequency, outputStreamCaptor.toString().trim());
-//    }
-//    @Test
-//    void doesMonthDelayMatchExpected(){
-//        Printer.monthDelay();
-//        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value - "+Settings.getMonthDelay, outputStreamCaptor.toString().trim());
-//    }
-//
-//    @Test
-//    void DoesAreValuesCorrectMatchExpected(){
-//        Printer.areValuesCorrect();
-//        Assertions.assertEquals("Are these values correct? (Y/N) ", outputStreamCaptor.toString().trim());
-//    }
-//
-//    @Test
-//    void DoesCurrentMonthMatchExpected(){
-//        Printer.currentMonth();
-//        Assertions.assertEquals("Current Month: "+TimeTracker.getCurrentMonth());
-//    }
-//
-//    @Test
-//    void DoesOpenCentresMatchExpected(){
-//        Printer.openCentres();
-//        Assertions.assertEquals("Number of open centres: "+ Company.getOpenCentres());
-//    }
-//
-//    @Test
-//    void DoesFullCentresMatchExpected(){
-//        Printer.fullCentres();
-//        Assertions.assertEquals("Number of full centres: "+ Company.getFullCentres());
-//    }
-//    @Test
-//    void DoesTraineesInTrainingMatchExpected(){
-//        Printer.traineesInTraining();
-//        Assertions.assertEquals("Number of trainees in training: "+ Company.getTraineesInTraining());
-//    }
-//    @Test
-//    void DoesTraineesInWaitingListMatchExpected(){
-//        Printer.traineesInWaitingList();
-//        Assertions.assertEquals("Number of trainees in waiting list: "+ Company.getTraineesInWaitingList());
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    @AfterEach
-//    public void tearDown(){
-//    System.setOut(standardOut);
-//}
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+@BeforeEach
+public void setUp(){
+    System.setOut(new PrintStream(outputStreamCaptor));
+}
+
+@Test
+    void doesGreetingStringMatchExpected(){
+    Printer.greeting();
+
+    Assertions.assertEquals("Welcome to the Sparta simulator", outputStreamCaptor.toString().trim());
+}
+
+    @Test
+    void doesDefaultSettingsMatchExpected(){
+        Printer.defaultSettings();
+        Assertions.assertEquals("The default values are: ", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void doesSimulationTimeMatchExpected(){
+        Printer.simulationTime();
+        Assertions.assertEquals("Enter simulation time in Months (Press enter to use default value: "+ Settings.getSimulationTime, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void doesCentreOpeningFrequencyMatchExpected(){
+        Printer.centreOpeningFrequency();
+        Assertions.assertEquals("Enter centre opening frequency (Press enter to use default value: "+ Settings.getCentreOpeningFrequency, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void doesCentreCapacityMatchExpected(){
+        Printer.centreCapacity();
+        Assertions.assertEquals("Enter centre capacity (Press enter to use default value: "+Settings.getCentreCapacity, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void doesNewTraineeMinMatchExpected(){
+        Printer.newTraineeMin();
+        Assertions.assertEquals("Enter lower bound for new trainee generation range (Press enter to use default value: "+Settings.getTraineeMin, outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void doesNewTraineeMaxMatchExpected(){
+        Printer.newTraineeMax();
+        Assertions.assertEquals("Enter upper bound for new trainee generation range (Press enter to use default value: "+Settings.getTraineeMax, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void doesNewTraineeFrequencyMatchExpected(){
+        Printer.newTraineeFrequency();
+        Assertions.assertEquals("Enter new trainee frequency in months (Press enter to use default value: "+Settings.getNewTraineeFrequency, outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void doesCentreAdmittanceMinMatchExpected(){
+        Printer.centreAdmittanceMin();
+        Assertions.assertEquals("Enter lower bound for centre admittance (Press enter to use default value: "+Settings.getCentreAdmittanceMin, outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void doesCentreAdmittanceMaxMatchExpected(){
+        Printer.centreAdmittanceMax();
+        Assertions.assertEquals("Enter upper bound for centre admittance (Press enter to use default value: "+Settings.getCentreAdmittanceMax, outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void doesCentreAdmittanceFrequencyMatchExpected(){
+        Printer.centreAdmittanceFrequency();
+        Assertions.assertEquals("Enter centre admittance frequency in months (Press enter to use default value: "+Settings.getCentreAdmittanceFrequency, outputStreamCaptor.toString().trim());
+    }
+    @Test
+    void doesMonthDelayMatchExpected(){
+        Printer.monthDelay();
+        Assertions.assertEquals("Enter how many seconds represent one month (Press enter to use default value - "+Settings.getMonthDelay, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void DoesAreValuesCorrectMatchExpected(){
+        Printer.areValuesCorrectToRun();
+        Assertions.assertEquals("Are these values correct? (Y/N) ", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void DoesCurrentMonthMatchExpected(){
+        Printer.currentMonth();
+        Assertions.assertEquals("Current Month: "+TimeTracker.getCurrentMonth());
+    }
+
+    @Test
+    void DoesOpenCentresMatchExpected(){
+        Printer.openCentres();
+        Assertions.assertEquals("Number of open centres: "+ Company.getOpenCentres());
+    }
+
+    @Test
+    void DoesFullCentresMatchExpected(){
+        Printer.fullCentres();
+        Assertions.assertEquals("Number of full centres: "+ Company.getFullCentres());
+    }
+    @Test
+    void DoesTraineesInTrainingMatchExpected(){
+        Printer.traineesInTraining();
+        Assertions.assertEquals("Number of trainees in training: "+ Company.getTraineesInTraining());
+    }
+    @Test
+    void DoesTraineesInWaitingListMatchExpected(){
+        Printer.traineesInWaitingList();
+        Assertions.assertEquals("Number of trainees in waiting list: "+ Company.getTraineesInWaitingList());
+    }
+
+    @AfterEach
+    public void tearDown(){
+    System.setOut(standardOut);
+}
 
 }
