f01880013c33d963085bd86f9aa8264a3a24bd07
Zahraa <zahraa_khalique@hotmail.co.uk>
2021-01-05 16:01:53 +0000

    Updating Centre.java and Trainee.java and creating CentreTest.java

--- src/main/java/com/sparta/model/Centre.java
+++ src/main/java/com/sparta/model/Centre.java
@@ -1,4 +1,44 @@
 package com.sparta.model;
 
+import com.sparta.utility.Settings;
+
+import java.util.ArrayList;
+
 public class Centre {
+    private int centreID;
+    private final int capacity= Settings.CENTRE_CAPACITY.getValue();
+    public static ArrayList<Trainee> trainees = new ArrayList();
+    public int traineeCount=0;
+
+    public static ArrayList<Trainee> getTraineesList(){
+        return trainees;
+    }
+
+    public int getNumberOfTrainees() {
+        int traineeInProgress=0;
+        for (Trainee trainee : trainees) {
+            traineeInProgress=trainee.getCount();
+        }
+        traineeCount=traineeInProgress;
+        return traineeInProgress;
+    }
+
+    public boolean isFull() {
+        if (getCapacityCount()>0) {
+            return false;
+        }
+        return true;
+    }
+
+    private int getCapacityCount () {
+        int currentCapacity=0;
+        currentCapacity = capacity - traineeCount;
+        return currentCapacity;
+    }
+
+    public void addTrainee(Trainee trainee)  {
+        trainees.add(trainee);
+    }
+
+
 }
--- src/main/java/com/sparta/model/Trainee.java
+++ src/main/java/com/sparta/model/Trainee.java
@@ -1,4 +1,17 @@
 package com.sparta.model;
 
 public class Trainee {
+    private int traineeID;
+    private static int count=0;
+    public Trainee() {
+        this.traineeID =count;
+        count++;
+    }
+    public int getTraineeID() {
+        return traineeID;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
