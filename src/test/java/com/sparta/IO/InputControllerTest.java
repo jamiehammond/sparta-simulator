3a8d0bd5749ebbed43f486a3176355f817e77e44
jamiehammond <97jamiehammond@gmail.com>
2021-01-06 15:01:35 +0000

    Commented out Test code.

--- src/test/java/com/sparta/IO/InputControllerTest.java
+++ src/test/java/com/sparta/IO/InputControllerTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class InputControllerTest {
 
-    @Test
-    void DoesYReturnTrue(){
-        Assertions.assertTrue(InputController.yesOrNo("Y"));
-    }
-    @Test
-    void DoesNReturnFalse(){
-        Assertions.assertFalse(InputController.yesOrNo("N"));
-    }
+//    @Test
+//    void DoesYReturnTrue(){
+//        Assertions.assertTrue(InputController.yesOrNo("Y"));
+//    }
+//    @Test
+//    void DoesNReturnFalse(){
+//        Assertions.assertFalse(InputController.yesOrNo("N"));
+//    }
 
 
 
