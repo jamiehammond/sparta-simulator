a9934c1ffd50695d98de9247340b1e4249093104
Ben Middlehurst <bmiddlehurst@spartaglobal.com>
2021-01-07 15:22:09 +0000

    Issue #74 & 76. Client class & methods updated

--- src/main/java/com/sparta/model/Client.java
+++ src/main/java/com/sparta/model/Client.java
@@ -1,5 +1,6 @@
 package com.sparta.model;
 
+import com.sparta.configuration.Settings;
 import com.sparta.utility.TimeTracker;
 
 import java.text.ParseException;
@@ -16,55 +17,37 @@ public class Client {
 
     private int clientID;
     private static int count=0;
-    private LinkedList clientRequirements = new LinkedList();
+    private LinkedList<ClientRequirements> clientRequirements;
     private ArrayList<Trainee> hiredTrainees;
     private boolean isClientHappy;
 
     public Client() {
         this.clientID =count;
         count++;
-        this.clientRequirements = clientRequirements;
-        addClientRequirements(clientRequirements);
-    }
-
-    private LinkedList addClientRequirements(LinkedList clientRequirements){
-        this.clientRequirements = clientRequirements;
-        clientRequirements.add(ClientRequirements.getStartDate);
-        clientRequirements.add(ClientRequirements.getCourseType);
-        clientRequirements.add(ClientRequirements.getTraineesToHire);
-        clientRequirements.add(true);
-        return clientRequirements;
+        clientRequirements = ClientRequirements.clientRequirements();
     }
 
     public void addHiredTrainee(Trainee trainee)  {
         hiredTrainees.add(trainee);
     }
-
-    private boolean isClientHappy(LinkedList clientRequirements) {
-        this.isClientHappy = isClientHappy;
-        Date startDate = new Date();
-        try {
-            startDate = new SimpleDateFormat("yyyy/MM/dd").parse(clientRequirements.get(0).toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
+    private void checkClientHappiness() {
+        for (ClientRequirements currentReq : clientRequirements) {
+            if (currentReq.isCompleted == false) {
+                LocalDate startDate = currentReq.getStartDate();
+                LocalDate endDate = startDate.addYears(Settings.REQUIREMENT_EXPIRY_TIME.getValue());
+                if (TimeTracker.getCurrentDate().isAfter(endDate)) {
+                    if (currentReq.getHiredTrainees.size() < currentReq.getTraineesToHire()) {
+                        isClientHappy = false;
+                        break;
+                    }
+                    else {
+                        currentReq.setCompleted(true);
+                        isClientHappy = true;
+                    }
+                }
+            }
         }
-        Calendar c = Calendar.getInstance();
-        c.setTime(startDate);
-        c.add(Calendar.YEAR, 1);
-        Date leaveDate = c.getTime();
-        leaveDate.toString();
-        ChronoLocalDate leaveDateTwo = LocalDate.parse(leaveDate.toString());
-
-        while (TimeTracker.getCurrentDate().isBefore(leaveDateTwo)){
-            isClientHappy = true;
-        }
-        if (hiredTrainees.size() < Integer.parseInt(clientRequirements.get(2).toString())){
-            isClientHappy = false;
-        }
-        clientRequirements.set(3, isClientHappy);
-        return isClientHappy;
     }
-
 
     public int getClientID() {
         return clientID;
@@ -72,7 +55,7 @@ public class Client {
     public int getCount() {
         return count;
     }
-    public LinkedList getClientRequirements() {return clientRequirements;}
+    public LinkedList<ClientRequirements> getClientRequirements() {return clientRequirements;}
     public ArrayList<Trainee> getHiredTrainees() {return hiredTrainees;}
     public boolean isClientHappy() {return isClientHappy;}
 
