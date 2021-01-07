b59ff9c51ae50f6cdbb585a379b878ffaf7019e0
jamiehammond <97jamiehammond@gmail.com>
2021-01-07 11:06:00 +0000

    Added constructor to CourseType.java.

--- src/main/java/com/sparta/model/Trainee.java
+++ src/main/java/com/sparta/model/Trainee.java
@@ -3,8 +3,11 @@ package com.sparta.model;
 public class Trainee {
     private int traineeID;
     private static int count=0;
+    private CourseType courseType;
+
     public Trainee() {
         this.traineeID =count;
+
         count++;
     }
     public int getTraineeID() {
@@ -13,5 +16,9 @@ public class Trainee {
 
     public int getCount() {
         return count;
+    }
+
+    public CourseType getCourseType() {
+        return courseType;
     }
 }
