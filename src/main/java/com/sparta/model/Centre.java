8e3704dc44c435ae66d5963c02589f7ca7b35822
sidkhosla <sidkhosla2@gmail.com>
2021-01-07 22:27:36 +0530

    Added Return the list of trainees in a course. Issue#80

--- src/main/java/com/sparta/model/Centre.java
+++ src/main/java/com/sparta/model/Centre.java
@@ -52,14 +52,18 @@ public abstract class Centre {
     public int getCentreId() {
         return centreId;
     }
-    public ArrayList<Trainee> getTraineesByCourseType(CourseType courseType){
-        ArrayList<Trainee> traineesWithGivenCourseType = new ArrayList<>();
-        for(int i=0;i<trainees.size();i++){
-            if(trainees.get(i).getCourseType() == courseType){
-                traineesWithGivenCourseType.add(trainees.get(i));
+    public ArrayList<Trainee> getTraineesByCourseType(CourseType courseType) {
+        if (this.courseType != courseType) {
+            ArrayList<Trainee> traineesWithGivenCourseType = new ArrayList<>();
+            for (int i = 0; i < trainees.size(); i++) {
+                if (trainees.get(i).getCourseType() == courseType) {
+                    traineesWithGivenCourseType.add(trainees.get(i));
+                }
             }
+            return traineesWithGivenCourseType;
+        }else{
+            return trainees;
         }
-        return traineesWithGivenCourseType;
     }
 
     public boolean isOverGracePeriod(){
